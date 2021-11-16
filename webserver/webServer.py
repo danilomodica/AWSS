3218ab90ffac0bf3b3880acd0d1021af3e8b253b
danilomodica <danilo.modica01@universitadipavia.it>
2021-11-16 17:25:38 +0100

    Added result page - temp

--- webserver/webServer.py
+++ webserver/webServer.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , request, send_file
+from flask import Flask , render_template , request, redirect, url_for, send_file
 import subprocess
 import os
 
@@ -14,6 +14,14 @@ def index():
 		file1.save('files\\file1.txt')
 		file2.save('files\\file2.txt')
 
-		subprocess.check_call(["exe\\Substring.exe",'files\\file1.txt', 'files\\file2.txt', "5"])
+		check = subprocess.check_call(["exe\\Substring.exe",'files\\file1.txt', 'files\\file2.txt', "5"])
+		return redirect(url_for('result'))
 
 	return render_template("index.html")
+
+@app.route('/result', methods=('GET','POST'))
+def result ():
+	if request.method == 'POST':
+		path = "res_output.txt"
+		return send_file(path, as_attachment=True)
+	return render_template("result.html")
