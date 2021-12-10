c378aebd9f7d21a0e7f03e5b0f55415f318dc74b
danilomodica <danilo.modica01@universitadipavia.it>
2021-12-10 15:59:13 +0100

    Added forms control (TEMP)

--- webserver/webServer.py
+++ webserver/webServer.py
@@ -9,17 +9,23 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
 
-        file1 = request.files['file1ToUpload']
-        file2 = request.files['file2ToUpload']
+        # To distinguish two forms in same route a check is needed, i.e. checking which of the two buttons has been clicked
+        if 'compute' in request.form:
+            file1 = request.files['file1ToUpload']
+            file2 = request.files['file2ToUpload']
 
-        file1.save('files\\file1.txt')
-        file2.save('files\\file2.txt')
+            file1.save('files\\file1.txt')
+            file2.save('files\\file2.txt')
 
-        random.seed(None, version=2)
-        filename = "result_"+str(random.randint(1,999999999999))+".txt"
-        check = subprocess.check_call(["exe\\Substring.exe",'files\\file1.txt', 'files\\file2.txt', "5", filename])
+            random.seed(None, version=2)
+            filename = "result_"+str(random.randint(1,999999999999))+".txt"
+            check = subprocess.check_call(["exe\\Substring.exe",'files\\file1.txt', 'files\\file2.txt', "5", filename])
 
-        return redirect(url_for('result', fileName=filename))
+            return redirect(url_for('result', fileName=filename))
+
+        if 'get' in request.form:
+            # TODO: get the file from S3 using the ID and send it back to the user
+            print('')
 
     return render_template("index.html")
 
