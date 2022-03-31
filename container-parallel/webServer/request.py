a07d78f500244b8d490943e72dcdbb69269b8642
gCass <guglielmo.cassini01@universitadipavia.it>
2022-03-31 10:10:34 +0200

    Docker file e relativo webserver

--- container-parallel/webServer/request.py
+++ container-parallel/webServer/request.py
@@ -1,8 +1,9 @@
 import requests
 
-url = 'http://127.0.0.1:5000'
-myobj = {'somekey': 'somevalue','bucket_in': 'prova-s3-bucke','file1':'files/file1.txt','file2':'files/file2.txt'}
+# url = 'http://127.0.0.1:5000'
+url = "http://3.71.74.117:5000"
+myobj = {'somekey': 'somevalue','bucket_in': 'prova-s3-bucke','file1':'files/file1.txt','file2':'files/file2.txt', "franco": "pesc"}
 
-x = requests.post(url, json = myobj)
+x = requests.post(url, json = myobj, timeout=30.00)
 
 print(x.text)
--- container-parallel/webServer/webServer.py
+++ container-parallel/webServer/webServer.py
@@ -12,19 +12,20 @@ def index():
 	data = request.json
 	s3 =  boto3.resource('s3')
 	bucket_in = s3.Bucket(data['bucket_in'])
-
+	exception_happened = False
 	try:
 		bucket_in.download_file(data['file1'],'myfile1.txt')
 		bucket_in.download_file(data['file2'],'myfile2.txt')
 	except Exception as e:
 		print(e)
+		exception_happened = True
 	
 	filename = "file_risultato.txt"
-	check = subprocess.check_call(["lcs.exe",'file1.txt', 'file2.txt', "5", filename])
-
-
-	return data
-
+	check = subprocess.check_call(["./lcs.exe",'myfile1.txt', 'myfile2.txt', "5", filename])
+	results = {**data, "exception_happened": True}	
+ 
+ 
+	return results
 
 '''
  nomi 2 bucket
