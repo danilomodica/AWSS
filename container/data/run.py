80d4c9502cd6cd47504b726d5f2fe84784162d3e
danilomodica <danilo.modica01@universitadipavia.it>
2022-05-02 18:08:53 +0200

    Code run.py for docker container

--- container/data/run.py
+++ container/data/run.py
@@ -1,94 +1,62 @@
-# Eseguito direttamente da il container che lo fa e poi si stoppa
+# The container runs automatically this script and stop after completion
 import boto3
 import botocore
 import subprocess
-import sys
 import os
 
-
-print("inizio", file=sys.stderr)
+print("Start")
 data = os.environ
-print("recupera variabili ambiente", file=sys.stderr)
+print("Retrieving environment variables")
 s3 =  boto3.resource('s3')
-print("istanzia s3 resource", file=sys.stderr)
+print("Instantiating S3 resource")
 bucket_in = s3.Bucket(data['bucket_in'])
-print("bucket lettura fatto", file=sys.stderr)
+print("Bucket has been read")
 exception_happened = False
-# Lettura file
+
+# Reading File
 exception_location = ""
 try:
     bucket_in.download_file(data['file1'],'myfile1.txt')
     bucket_in.download_file(data['file2'],'myfile2.txt')
 except Exception as e:
     print(e)
-    print(e, file=sys.stderr)
-    print(e, file=sys.stdout)
     exception_happened = True
     exception_location = "During download of files"	
-
-print("lettura fatta")
+print("Files have been read")
 
 if not exception_happened:
-    #Esecuzione lcs su c con openmp	  
-    filename = "file_risultato.txt"
+    # Executing lcs code with C/OpenMP	  
+    filename = "output_file.txt"
     check = subprocess.check_call(["./lcs.exe",'myfile1.txt', 'myfile2.txt', "5", filename])
-    # Scrittura dei file nel bucket dei risultati
-    print("lcs fatto", file=sys.stderr)
+    print("LCS code executed")
+
+    # Writing output file into results bucket
     try:
         with open(filename, "rb") as f:
-            # response = s3_client.upload_file(f, data["bucket_out"], data["result_file"])
-            # s3.upload_fileobj(f, data["bucket_out"], data["result_file"])
             txt_data = f.read()
             object = s3.Object(data["bucket_out"], data["result_file"])
             result = object.put(Body=txt_data)
     except Exception as e:
         print(e)
-        print(e, file=sys.stderr)
-        print(e, file=sys.stdout)
         exception_happened = True
         exception_location = "During upload of results"
-    print("scrittura risultato fatto", file=sys.stderr)
+    print("Result has been stored")
 
 results = {**data, "exception_happened": exception_happened, "exception_location": exception_location}	
 print(results)
 
-
-# Inserimento nella coda dei risultati
+# Adding message into the SQS queue
 AWS_REGION = "eu-central-1"
 sqs_client = boto3.client("sqs", region_name=AWS_REGION)
 
-
-def send_queue_message(queue_url, msg_attributes, msg_body):
-    """
-    Sends a message to the specified queue.
-    """
-    try:
-        response = sqs_client.send_message(QueueUrl=queue_url,
-                                           MessageAttributes=msg_attributes,
-                                           MessageBody=msg_body)
-    except botocore.exceptions.ClientError:
-        # logger.exception(f'Could not send meessage to the - {queue_url}.')
-        raise
-    else:
-        return response
-
-# CONSTANTS
+# Constants definition
 QUEUE_URL = data["queue_url"]
-
-# MSG_ATTRIBUTES = {
-#     'Title': {
-#         'DataType': 'String',
-#         'StringValue': 'Working with SQS in Python using Boto3'
-#     },
-#     'Author': {
-#         'DataType': 'String',
-#         'StringValue': 'Abhinav D'
-#     }
-# }
 MSG_ATTRIBUTES = {}
-
 id = data["result_file"].split(".")[0]
-
 MSG_BODY = f'{id} {data["email"]} 1'
 
-msg = send_queue_message(QUEUE_URL, MSG_ATTRIBUTES, MSG_BODY)
+try:
+    response = sqs_client.send_message(QueueUrl=queue_url, MessageAttributes=msg_attributes, MessageBody=msg_body)
+except botocore.exceptions.ClientError:
+    raise
+print("Message added to the SQS queue")
