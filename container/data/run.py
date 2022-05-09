e9aca925875061ab14559959cac38c3bebb16747
domenico-rgs <domenico.ragusa01@universitadipavia.it>
2022-05-09 19:17:59 +0200

    run.py bug fixes + return to origin (ubuntu)

--- container/data/run.py
+++ container/data/run.py
@@ -1,7 +1,6 @@
 # The container runs automatically this script and stops after completion
 import boto3
 import botocore
-import subprocess
 import os
 import signal
 import re
@@ -41,31 +40,30 @@ try:
     textfile2 = open("myfile2.txt", 'r')
 
     if(check_string(textfile1) == False or check_string(textfile2) == False):
-        MSG_BODY = f'{id} {data["email"]} 0 {"Error: Recheck the string you have inserted"}'
+        MSG_BODY = f'{id}-{data["email"]}-0-ERROR: Recheck the string you have inserted'
         textfile1.close()
         textfile2.close()
     else:
         textfile1.close()
         textfile2.close()
         filename = "output_file.txt"
-        
-        check = subprocess.check_call(["./lcs",'myfile1.txt', 'myfile2.txt', "5", filename])
+        check = os.system("./lcs myfile1.txt myfile2.txt 5 "+ filename)
     
         if check == 0:
             with open(filename, "rb") as f:
                 txt_data = f.read()
                 object = s3.Object(data["bucket_out"], data["result_file"])
                 object.put(Body=txt_data)
-            MSG_BODY = f'{id} {data["email"]} 1'
-            print("Result has been stored")	
+            MSG_BODY = f'{id}-{data["email"]}-1'
+            print("SUCCESS: Result has been stored")	
         else:
             print(check)
-            MSG_BODY = f'{id} {data["email"]} 0 {"Error: Something went wrong during execution. Please contact us."}'
+            MSG_BODY = f'{id}-{data["email"]}-0-ERROR: Something went wrong during execution. Please contact us.'
 except TimeOutException:
     os.killpg(os.getpgid(check.pid), signal.SIGTERM)
-    MSG_BODY = f'{id} {data["email"]} 0 {"Error: Timeout"}'
+    MSG_BODY = f'{id}-{data["email"]}-0-ERROR: Timeout'
 except Exception as e:
     print(e)
-    MSG_BODY = f'{id} {data["email"]} 0 {"Error: Generic error"}'
+    MSG_BODY = f'{id}-{data["email"]}-0-ERROR: Generic error'
 
 sqs_client.send_message(QueueUrl=QUEUE_URL, MessageAttributes=MSG_ATTRIBUTES, MessageBody=MSG_BODY)
