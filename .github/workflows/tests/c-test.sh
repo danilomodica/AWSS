a9b6bf7b3ef12a05732c2153a0641bef34e83a71
domenico-rgs <domenico.ragusa01@universitadipavia.it>
2022-05-24 16:38:05 +0200

    Improve c tests

--- .github/workflows/tests/c-test.sh
+++ .github/workflows/tests/c-test.sh
@@ -1,10 +1,8 @@
-# Unique Test
-#make test file
 printf "atcg" > test1.txt 
 printf "atcgtc" > test2.txt 
-#make expected result file
 printf "atcg" > result.txt 
-# run alghorithm
 ./lcs test1.txt test2.txt 5 out.txt
-#compare results
-diff result.txt out.txt
+diff result.txt out.txt >> countLine
+$counter=`wc -l < countLine`;
+if [ $counter -ne 0 ]; then echo "Error"; fi
+#in case other tests are added substitute 0 with the number of expected line (one per failed test)
