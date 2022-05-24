6de25a81b093c047fd23eb813a498b1fbad01fe0
domenico-rgs <domenico.ragusa01@universitadipavia.it>
2022-05-24 18:30:52 +0200

    Correct lcs artifact download path

--- .github/workflows/tests/c-test.sh
+++ .github/workflows/tests/c-test.sh
@@ -3,6 +3,6 @@ printf "atcgtc" > test2.txt
 printf "atcg" > result.txt 
 ./lcs test1.txt test2.txt 5 out.txt
 diff result.txt out.txt >> countLine
-$counter=`wc -l < countLine`;
-if [ $counter -ne 0 ]; then echo "Error"; fi
+counter=$(wc -l < countLine)
+if [ "$counter" -ne 0 ]; then echo "Error"; fi
 #in case other tests are added substitute 0 with the number of expected line (one per failed test)
