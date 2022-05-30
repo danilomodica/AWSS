9d1af8e8543d7ccaa774ab7c75a7111e19a5b565
domenico-rgs <domenico.ragusa01@universitadipavia.it>
2022-05-30 19:02:32 +0200

    Description to the tf variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ terraform {
     }
   }
 
+  #Terraform cloud set-up
   cloud {
     organization = "AWSS-Cloud"
 
@@ -27,19 +28,24 @@ variable "service_name" { default = "awss" }
 variable "website_url" { default = "awss-cloud.ga" }
 
 #Credentials to access to OpenSearch dashboard
-variable "masterName" { default = "awssCloud" }
-variable "masterPass" { default = "awssCC22*" }
-
-output "OpenSearch_credentials" {
-  value = [var.masterName, var.masterPass]
+variable "masterName" { 
+  description = "Username for OpenSearch" 
+  default = "awssCloud" 
 }
+variable "masterPass" { description = "Password for OpenSearch"}
 
 #Email variables to send notify to users through smtp and notify developers in case of alarms
 variable "email" { default = "awss.unipv@gmail.com" }
 variable "gmail" { description = "Gmail password" }
 
-variable "acm_certificate_arn" { default = "arn:aws:acm:us-east-1:389487414326:certificate/72c48e73-7243-4df2-87ca-cb1ad8f82172" } #arn certificato SSL/TLS (creato a priori)
-variable "route_zone_id" { default = "Z06871683OMTQJ90WZ6D5" }
+variable "acm_certificate_arn" { 
+  description = "ARN of the SSL/TLS certificate created at priori"
+  default = "arn:aws:acm:us-east-1:389487414326:certificate/72c48e73-7243-4df2-87ca-cb1ad8f82172" 
+}
+variable "route_zone_id" { 
+  description = "ID of the Route53 hosted zone"
+  default = "Z06871683OMTQJ90WZ6D5" 
+}
 
 data "aws_caller_identity" "current" {} # to take iam ID
 
